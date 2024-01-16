11141546ddcf45d612b62c8a19b6b7ca885dbba6
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-01-15 17:34:50 -0700

    zip commented

--- zip.go
+++ zip.go
@@ -1,10 +1,10 @@
 package iter
 
-type Tuple2[T, U any] {
-	V1 T
-	V2 U
-}
-
-func Zip2[T, U any](i1 Iter[T], i2 Iter[U]) *Iterator[Pair[T, U]] {
-	i2 := 
-}
+// type Tuple2[T, U any] {
+// 	V1 T
+// 	V2 U
+// }
+// 
+// func Zip2[T, U any](i1 Iter[T], i2 Iter[U]) *Iterator[Pair[T, U]] {
+// 	i2 := 
+// }
